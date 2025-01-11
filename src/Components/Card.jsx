2f998c01a797c4d95a9d566cf5c7ef5c3a6a3c02
ankitkumargupta <ankitgupta6881@gmail.com>
2025-01-11 22:17:16 +0530

    made add song to fav feature using props in react

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -10,7 +10,7 @@ function Card({song, songIndex, handleClick}) {
         <img className='w-full h-full object-cover' src={image} alt="" />
       </div>
       <div>
-        <h3 className='text-xl font-semibold'>{name}</h3>
+        <h3 className='text-xl font-semibold font-sans'>{name}</h3>
         <h3 className='text-xs'>{artist}</h3>
       </div>
       <button onClick={()=>handleClick(songIndex)} className={`px-4 py-2 ${added === false ? "bg-orange-500" : "bg-teal-500"} text-white text-xs rounded-full whitespace-nowrap absolute bottom-0 left-1/2 -translate-x-[50%] translate-y-[50%]`}>{added === false ? "Add to Favourites" : "Added"}</button>
